559aa11398b34bcc82dd82c41eda1897e05a4b6f
esvault <azarnikov01@gmail.com>
2024-05-03 15:53:16 +0300

    added spsa2_sched

--- drivers/cpufreq/cpufreq_spsa2_sched.c
+++ drivers/cpufreq/cpufreq_spsa2_sched.c
@@ -22,6 +22,11 @@
 #include "cpufreq_governor.h"
 #include "cpufreq_spsa2.h"
 #include <linux/random.h>
+#include <linux/string.h>
+
+
+#include <linux/sched/signal.h>
+#include <linux/sched.h>
 
 /* On-demand governor macros */
 #define DEF_FREQUENCY_UP_THRESHOLD		(80)
@@ -109,7 +114,7 @@ static unsigned int generic_powersave_bias_target(struct cpufreq_policy *policy,
 	return freq_hi;
 }
 
-static void spsa2_powersave_bias_init(struct cpufreq_policy *policy)
+static void spsa2_sched_powersave_bias_init(struct cpufreq_policy *policy)
 {
 	struct od_policy_dbs_info *dbs_info = to_dbs_info(policy->governor_data);
 
@@ -370,10 +375,37 @@ static int determine_new_freq(struct cpufreq_policy* policy, struct spsa2_policy
 		    pr_warn("gov spsa2, frequency %u is not found for cpu %u \n", current_freq, policy->cpu);
 		index = 0;
 	}
-
 	
-    model = 0; 
+//	if(dbs_info->requested_freq != 0 && dbs_info->requested_freq != current_freq)
+//	{
+//        int index;
+//		// we wait CPU to switch
+//		//dbs_info->load_sum += load;
+//		//dbs_info->load_count += 1;
+//
+//        //TODO may have trash in dbs_info->requested_freq
+//        for(index = 0; index < freq_amount; index++)
+//        {
+//            if (dbs_info->requested_freq == freq[index])
+//            {
+//                pr_warn("gov spsa2, waiting cpu %u to switch from %u to %u \n", policy->cpu, current_freq, dbs_info->requested_freq);
+//		        return dbs_info->requested_freq;
+//            }
+//        }
+//
+//		pr_warn("gov spsa2, waiting cpu %u to switch from %u to %u, but it is wrong freq (set %u freq as target) \n", policy->cpu, current_freq, dbs_info->requested_freq, freq[freq_amount / 2]);
+//        dbs_info->requested_freq = freq[freq_amount / 2]; 		
+//        return dbs_info->requested_freq;
+//	}
+	
+    model = 0; //calculate_functional(load, index, freq, en, freq_amount, target_load);
     new_index = index;
+    //////////////////////////////03.02.2023
+    /// test 1 - only phase 0, calculate to functionals
+    /// test 2 - 3 phase:
+    ///// 0 - calculate minus  (and run freq)
+    ///// 1 - calculate plus   (and run freq)
+    ///// 2 - calculate gradient from prev 2 calcs and run the freq obtained
  
     if (dbs_info->old_index < 0 || dbs_info->old_index > (freq_amount - 1))
         dbs_info->old_index = index;
@@ -469,7 +501,370 @@ static int determine_new_freq(struct cpufreq_policy* policy, struct spsa2_policy
 }
 
 // SPSA2 functionality ended
+
+// ================================ SPSA2_SCHED ================================ //
+#define MAX_PROC_NUM 14
+
+typedef enum app_type 
+{
+    AUDIO,
+    VIDEO,
+    CAMERA,
+    TYPE,
+    GAME,
+    ANOTHER
+} app_type;
+
+struct task_cons {
+	int pid;
+	char comm[TASK_COMM_LEN];
+	unsigned int cpu;
+	unsigned long pcount;                 // times process have run on this CPU 
+	unsigned long long exec_start;        // starting time of the process in the last scheduling tick period
+	unsigned long long sum_exec_runtime;  // total runtime of the process till now
+	unsigned long long vruntime;		  // virtual runtime
+};
+
+struct spsa2_sched_staff {
+	unsigned int phase;
+	unsigned long long dvfs_iter_count;
+
+	unsigned int left_snapshot_len;
+	unsigned int right_snapshot_len;
+
+	struct task_cons left_snapshot[MAX_PROC_NUM];
+	struct task_cons right_snaphot[MAX_PROC_NUM];
+};
+
+// prosess' cmd
+static char aimp_comm[TASK_COMM_LEN] = "com.aimp.player";
+static char mx_player_comm[TASK_COMM_LEN] = ".videoplayer.ad";
+static char note_comm[TASK_COMM_LEN] = "appmindlab.nano";
+static char hill_climb_comm[TASK_COMM_LEN] = "rsoft.hillclimb";
+static char open_camera_comm[TASK_COMM_LEN] = "orge.opencamera";
+static char audio_service_mediatek[TASK_COMM_LEN] = "ervice.mediatek";
+static char audioserver[TASK_COMM_LEN] = "audioserver";
+static char mediaserver[TASK_COMM_LEN] = "mediaserver";
+static char mediaswcodec[TASK_COMM_LEN] = "in/mediaswcodec";
+static char graphics_service[TASK_COMM_LEN] = "ser@2.1-service";
+static char mediatek_service[TASK_COMM_LEN] = ".pq@2.2-service";
+static char inputmethod[TASK_COMM_LEN] = "putmethod.latin";
+static char camerahalserver[TASK_COMM_LEN] = "camerahalserver";
+static char cameraserver[TASK_COMM_LEN] = "cameraserver";
+
+
+static struct spsa2_sched_staff spsa2_sched_staff;
+
+static int is_needed_task(struct task_struct *task) 
+{
+	char *comm = task->comm;
+
+	if      (strncmp(comm, aimp_comm, TASK_COMM_LEN) == 0)
+		return 1;
+	else if (strncmp(comm, mx_player_comm, TASK_COMM_LEN) == 0)
+		return 1;
+	else if (strncmp(comm, note_comm, TASK_COMM_LEN) == 0)
+		return 1;
+	else if (strncmp(comm, hill_climb_comm, TASK_COMM_LEN) == 0)
+		return 1;
+	else if (strncmp(comm, open_camera_comm, TASK_COMM_LEN) == 0)
+		return 1;
+	// else if (strncmp(comm, audioserver, TASK_COMM_LEN) == 0)
+	// 	return 1;
+	// else if (strncmp(comm, audio_service_mediatek, TASK_COMM_LEN) == 0)
+	// 	return 1;
+	// else if (strncmp(comm, mediaserver, TASK_COMM_LEN) == 0)
+	// 	return 1;
+	// else if (strncmp(comm, mediaswcodec, TASK_COMM_LEN) == 0)
+	// 	return 1;
+	// else if (strncmp(comm, graphics_service, TASK_COMM_LEN) == 0)
+	// 	return 1;
+	// else if (strncmp(comm, mediatek_service, TASK_COMM_LEN) == 0)
+	// 	return 1;
+	// else if (strncmp(comm, inputmethod, TASK_COMM_LEN) == 0)
+	// 	return 1;
+	// else if (strncmp(comm, camerahalserver, TASK_COMM_LEN) == 0)
+	// 	return 1;
+	// else if (strncmp(comm, cameraserver, TASK_COMM_LEN) == 0)
+	// 	return 1;
+	
+	return 0;
+}
+
+static void print_task_info(struct task_struct *task) 
+{
+	int pid = task_pid_nr(task);
+	unsigned int cpu = task->cpu;
+
+	struct sched_info sched_info = task->sched_info;
+	struct sched_entity se = task->se;
+	char comm[TASK_COMM_LEN];
+
+	get_task_comm(comm, task);
+
+	pr_warn("[[print_task_info]] %s[%d], CPU: %u, PCOUNT: %lu, RUNTIME: %llu", comm, pid, cpu, sched_info.pcount, se.sum_exec_runtime);
+	
+
+	// pr_warn("%s[%d], CPU: %u, SCHED_ENTITY: %llu exec_start, %llu sum_exec_runtime, %llu vruntime", 
+	// 		comm, pid, cpu, se.exec_start, se.sum_exec_runtime, se.vruntime);
+	
+	// pr_warn("SCHED_INFO: %lu times run on CPU, %llu waiting on RQ, %llu last run on CPU", 
+	// 		sched_info.pcount, sched_info.run_delay, sched_info.last_arrival);
+
+}
+
+static void print_tasks_cons_info(struct task_cons *tasks, int tasks_n) 
+{
+	int i;
+	struct task_cons t;
+
+	for (i = 0; i < tasks_n; ++i) {
+		t = tasks[i];
+		pr_warn("[[print_tasks_cons_info]] %s[%d], CPU: %u, PCOUNT: %lu, RUNTIME: %llu", t.comm, t.pid, t.cpu, t.pcount, t.sum_exec_runtime);
+	}
+}
+
+static struct task_cons task2task_cons(struct task_struct* task)
+{
+	struct task_cons result;
+
+	struct sched_info sched_info = task->sched_info;
+	struct sched_entity se = task->se;
+
+	result.pid = task_pid_nr(task);
+	get_task_comm(result.comm, task);
+
+	result.cpu = task->cpu;
+	result.pcount = sched_info.pcount;
+	result.exec_start = se.exec_start;
+	result.sum_exec_runtime = se.sum_exec_runtime;
+	result.vruntime = se.vruntime;
+
+	pr_warn("[[task2task_cons]] %s[%d], CPU: %u, PCOUNT: %lu, RUNTIME: %llu", result.comm, result.pid, result.cpu, result.pcount, result.sum_exec_runtime);
+
+	return result;
+}
+
+static int get_needed_tasks(struct task_cons *tasks)
+{
+	struct task_struct *p;
+
+	unsigned tasks_n = 0;
+	rcu_read_lock();
+    for_each_process(p) {
+		if (is_needed_task(p)) {
+			// needed_tasks[tasks_n] = task2task_cons(p);
+			print_task_info(p);
+			tasks[tasks_n] = task2task_cons(p);
+			tasks_n += 1;
+		}
+    }
+	rcu_read_unlock();
+
+	return tasks_n;
+}
+
+
+static struct task_cons calculate_task_cons_delta(struct task_cons *before_task_cons, struct task_cons *after_task_cons)
+{
+	struct task_cons delta;
+
+
+	delta.pid = after_task_cons->pid;
+	strcpy(delta.comm, after_task_cons->comm);
+	delta.cpu = after_task_cons->cpu;
+
+	delta.pcount = after_task_cons->pcount - before_task_cons->pcount;
+	delta.exec_start = after_task_cons->exec_start - before_task_cons->exec_start;
+	delta.sum_exec_runtime = after_task_cons->sum_exec_runtime - before_task_cons->sum_exec_runtime;
+	delta.vruntime = after_task_cons->vruntime - before_task_cons->vruntime;
+
+	return delta;
+}
+
+static app_type comm2app_type(const char *comm) 
+{
+	if (strncmp(comm, aimp_comm, TASK_COMM_LEN) == 0)
+		return AUDIO;
+
+	if (strncmp(comm, mx_player_comm, TASK_COMM_LEN) == 0)
+		return VIDEO;
+
+	if (strncmp(comm, note_comm, TASK_COMM_LEN) == 0)
+		return TYPE;
+
+	if (strncmp(comm, hill_climb_comm, TASK_COMM_LEN) == 0)
+		return GAME;
+
+	if (strncmp(comm, open_camera_comm, TASK_COMM_LEN) == 0)
+		return CAMERA;
+
+	return ANOTHER;
+}
+
+static const char *app_type2app_str(app_type app)
+{
+	switch (app)
+	{
+	case AUDIO:
+		return aimp_comm;
+		break;
+
+	case VIDEO:
+		return mx_player_comm;
+		break;
+
+	case CAMERA:
+		return open_camera_comm;
+		break;
+
+	case TYPE:
+		return note_comm;
+		break;
+
+	case GAME:
+		return hill_climb_comm;
+		break;
+
+	case ANOTHER:
+		return "unknown app";
+		break;
+	default:
+		return "unknown app";
+		break;
+	}
+    
+}
+
+static app_type detect_app_type(struct task_cons *before_snapshot, struct task_cons *after_snapshot, int before_n, int after_n) 
+{
+	struct task_cons diff_results[after_n];
+
+	int i, j;
+	struct task_cons diff_cons;
+	char *max_comm_by_runtime;
+	int max_runtime = 0, k = 0, prev_k = k;
+
+	for (i = 0; i < after_n; ++i) {
+		int pid = after_snapshot[i].pid;
+
+		prev_k = k;
+		for (j = 0; j < before_n; ++j) {
+			if (pid == before_snapshot[j].pid) {
+				diff_cons = calculate_task_cons_delta(before_snapshot + j, after_snapshot + i);
+				diff_results[k] = diff_cons;
+
+				pr_warn("[[DETECT_APP]] difference PID: %d, comm: %s, runtime diff: %llu", diff_cons.pid, diff_cons.comm, diff_cons.sum_exec_runtime);
+				k += 1;
+				break;
+			}
+		}
+
+		if (prev_k == k) {
+			diff_results[k] = after_snapshot[i];
+			k += 1;
+		}
+	}
+
+	for (i = 0; i < k; ++i) {
+		diff_cons = diff_results[i];
+		if (diff_cons.sum_exec_runtime > max_runtime) {
+			max_runtime = diff_cons.sum_exec_runtime;
+			max_comm_by_runtime = diff_cons.comm;
+		}
+	}
+
+	return comm2app_type(max_comm_by_runtime);
+}
+
+static void update_tuners(struct od_dbs_tuners *od_tuners, app_type app) 
+{
+	int a, b;
+	unsigned int tl;
+
+	if (app == VIDEO || app == TYPE || app == GAME) {
+		a = 4; b = 1; tl = 90;
+	} else if (app == AUDIO) {
+		a = 5; b = 1; tl = 90;
+	} else if (app == CAMERA) {
+		a = 5; b = 4; tl = 85;
+	} else {
+		a = 2; b = 1; tl = 80;
+	}
+
+	od_tuners->alpha = a;
+	od_tuners->beta = b;
+	od_tuners->target_load = tl;
+
+	pr_warn("+++++++++++++++++++ New a: %d, b: %d, target load: %lu +++++++++++++++++++", a, b, tl);
+}
+
+static void working_procedure(struct od_dbs_tuners *od_tuners)
+{
+	app_type app;
+	int before_snap_len, after_snap_len;
+	struct task_cons *before_snap;
+	struct task_cons *after_snap;
+
+	pr_warn("Entering in working_procedure ...");
+	switch (spsa2_sched_staff.phase)
+	{
+	case 0:
+		spsa2_sched_staff.left_snapshot_len = get_needed_tasks(spsa2_sched_staff.left_snapshot);
+		spsa2_sched_staff.phase = 1;
+		return;
+	case 1:
+		before_snap_len = spsa2_sched_staff.left_snapshot_len;
+		after_snap_len = get_needed_tasks(spsa2_sched_staff.right_snaphot);
+
+		spsa2_sched_staff.right_snapshot_len = after_snap_len;
+
+		before_snap = spsa2_sched_staff.left_snapshot;
+		after_snap = spsa2_sched_staff.right_snaphot;
+
+		app = detect_app_type(before_snap, after_snap, before_snap_len, after_snap_len);
+
+		spsa2_sched_staff.phase = 2;
+		break;
+	case 2:
+		before_snap_len = spsa2_sched_staff.right_snapshot_len;
+		after_snap_len = get_needed_tasks(spsa2_sched_staff.left_snapshot);
+
+		spsa2_sched_staff.left_snapshot_len = after_snap_len;
+
+		before_snap = spsa2_sched_staff.right_snaphot;
+		after_snap = spsa2_sched_staff.left_snapshot;
+		
+		app = detect_app_type(before_snap, after_snap, before_snap_len, after_snap_len);
+
+		spsa2_sched_staff.phase = 1;
+		break;
+
+	default:
+		break;
+	}
+
+	pr_warn("[[WORKING_PROCEDURE]] Detected app: %s", app_type2app_str(app));
+	update_tuners(od_tuners, app);
+}
+
+
 //////////////////////////////////////////////////////////////////
+static void dbs_freq_increase(struct cpufreq_policy *policy, unsigned int freq)
+{
+	struct policy_dbs_info *policy_dbs = policy->governor_data;
+	struct dbs_data *dbs_data = policy_dbs->dbs_data;
+	struct od_dbs_tuners *od_tuners = dbs_data->tuners;
+
+	if (od_tuners->powersave_bias)
+		freq = od_ops.powersave_bias_target(policy, freq,
+				CPUFREQ_RELATION_H);
+	else if (policy->cur == policy->max)
+		return;
+
+	__cpufreq_driver_target(policy, freq, od_tuners->powersave_bias ?
+			CPUFREQ_RELATION_L : CPUFREQ_RELATION_H);
+}
 
 /*
  * Every sampling_rate, we check, if current idle time is less than 20%
@@ -478,6 +873,8 @@ static int determine_new_freq(struct cpufreq_policy* policy, struct spsa2_policy
  */
 static void od_update(struct cpufreq_policy *policy)
 {
+	app_type app;
+	int first_tasks_n, second_tasks_n;
 	struct policy_dbs_info *policy_dbs = policy->governor_data;
 	struct od_policy_dbs_info *dbs_info = to_dbs_info(policy_dbs);
 	struct dbs_data *dbs_data = policy_dbs->dbs_data;
@@ -486,7 +883,16 @@ static void od_update(struct cpufreq_policy *policy)
 
 	unsigned int freq_next, cluster;
 	struct spsa2_policy_dbs_info* dbs_info_spsa;
+	// struct spsa2_sched_staff* staff;
 
+
+	// pr_warn("================================= UPD_TIMER: %lu, UPD_COUNTER: %d =================================", upd_timer, upd_counter);
+	
+	if (spsa2_sched_staff.dvfs_iter_count % 1500 == 0)
+		working_procedure(od_tuners);
+
+	spsa2_sched_staff.dvfs_iter_count += 1;
+	
 	dbs_info->freq_lo = 0;
 
 	cluster = policy->cpu < 6 ? 0 : 1;
@@ -498,6 +904,7 @@ static void od_update(struct cpufreq_policy *policy)
 
 	return;	
 }
+
 
 static unsigned int od_dbs_update(struct cpufreq_policy *policy)
 {
@@ -639,7 +1046,7 @@ static ssize_t store_powersave_bias(struct gov_attr_set *attr_set,
 	od_tuners->powersave_bias = input;
 
 	list_for_each_entry(policy_dbs, &attr_set->policy_list, list)
-		spsa2_powersave_bias_init(policy_dbs->policy);
+		spsa2_sched_powersave_bias_init(policy_dbs->policy);
 
 	return count;
 }
@@ -797,10 +1204,10 @@ static int od_init(struct dbs_data *dbs_data)
 	dbs_data->io_is_busy = should_io_be_busy();
 
 	// spsa2 use
-	tuners->alpha = 4;
+	tuners->alpha = 2;
 	tuners->beta = 1;
-	tuners->target_load = 90;
-	tuners->log_print = 1;
+	tuners->target_load = 80;
+	tuners->log_print = 0;
 	// spsa2 use end
 
 	dbs_data->tuners = tuners;
@@ -819,6 +1226,10 @@ static int od_init(struct dbs_data *dbs_data)
         clusters_data[cluster].phase = 0;
         clusters_data[cluster].old_load = 100;   
     }
+
+	spsa2_sched_staff.dvfs_iter_count = 0;
+	spsa2_sched_staff.phase = 0;
+
 	return 0;
 }
 
@@ -832,7 +1243,7 @@ static void od_start(struct cpufreq_policy *policy)
 	struct od_policy_dbs_info *dbs_info = to_dbs_info(policy->governor_data);
 
 	dbs_info->sample_type = OD_NORMAL_SAMPLE;
-	spsa2_powersave_bias_init(policy);
+	spsa2_sched_powersave_bias_init(policy);
 }
 
 static struct od_ops od_ops = {
@@ -840,7 +1251,7 @@ static struct od_ops od_ops = {
 };
 
 static struct dbs_governor od_dbs_gov = {
-	.gov = CPUFREQ_DBS_GOVERNOR_INITIALIZER("spsa2"),
+	.gov = CPUFREQ_DBS_GOVERNOR_INITIALIZER("spsa2_sched"),
 	.kobj_type = { .default_attrs = od_attributes },
 	.gov_dbs_update = od_dbs_update,
 	.alloc = od_alloc,
@@ -850,7 +1261,7 @@ static struct dbs_governor od_dbs_gov = {
 	.start = od_start,
 };
 
-#define CPU_FREQ_GOV_ONDEMAND	(&od_dbs_gov.gov)
+#define CPU_FREQ_GOV_SPSA2_SCHED	(&od_dbs_gov.gov)
 
 static void od_set_powersave_bias(unsigned int powersave_bias)
 {
@@ -871,7 +1282,7 @@ static void od_set_powersave_bias(unsigned int powersave_bias)
 			continue;
 
 		policy = cpufreq_cpu_get_raw(cpu);
-		if (!policy || policy->governor != CPU_FREQ_GOV_ONDEMAND)
+		if (!policy || policy->governor != CPU_FREQ_GOV_SPSA2_SCHED)
 			continue;
 
 		policy_dbs = policy->governor_data;
@@ -887,30 +1298,30 @@ static void od_set_powersave_bias(unsigned int powersave_bias)
 	put_online_cpus();
 }
 
-void od_register_powersave_bias_handler_spsa2(unsigned int (*f)
+void od_register_powersave_bias_handler_spsa2_sched(unsigned int (*f)
 		(struct cpufreq_policy *, unsigned int, unsigned int),
 		unsigned int powersave_bias)
 {
 	od_ops.powersave_bias_target = f;
 	od_set_powersave_bias(powersave_bias);
 }
-EXPORT_SYMBOL_GPL(od_register_powersave_bias_handler_spsa2);
+EXPORT_SYMBOL_GPL(od_register_powersave_bias_handler_spsa2_sched);
 
-void od_unregister_powersave_bias_handler_spsa2(void)
+void od_unregister_powersave_bias_handler_spsa2_sched(void)
 {
 	od_ops.powersave_bias_target = generic_powersave_bias_target;
 	od_set_powersave_bias(0);
 }
-EXPORT_SYMBOL_GPL(od_unregister_powersave_bias_handler_spsa2);
+EXPORT_SYMBOL_GPL(od_unregister_powersave_bias_handler_spsa2_sched);
 
 static int __init cpufreq_gov_dbs_init(void)
 {
-	return cpufreq_register_governor(CPU_FREQ_GOV_ONDEMAND);
+	return cpufreq_register_governor(CPU_FREQ_GOV_SPSA2_SCHED);
 }
 
 static void __exit cpufreq_gov_dbs_exit(void)
 {
-	cpufreq_unregister_governor(CPU_FREQ_GOV_ONDEMAND);
+	cpufreq_unregister_governor(CPU_FREQ_GOV_SPSA2_SCHED);
 }
 
 MODULE_AUTHOR("Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>");
@@ -922,7 +1333,7 @@ MODULE_LICENSE("GPL");
 #ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND
 struct cpufreq_governor *cpufreq_default_governor(void)
 {
-	return CPU_FREQ_GOV_ONDEMAND;
+	return CPU_FREQ_GOV_SPSA2_SCHED;
 }
 
 fs_initcall(cpufreq_gov_dbs_init);
